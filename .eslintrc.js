7d1ebd66e60b0f3132d5ee61833364072e4bb54d
chornos13 <chornos13@hotmail.com>
2022-02-11 01:07:39 +0700

    chore: fix setup eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,14 @@ module.exports = {
   env: {
     es6: true,
     node: true,
+    // resolve jest global variable in test files (test, describe, .etc)
+    jest: true,
   },
-  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:prettier/recommended',
+  ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -12,7 +18,10 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
+    // https://github.com/iamturns/eslint-config-airbnb-typescript#i-get-this-error-when-running-eslint-the-file-must-be-included-in-at-least-one-of-the-projects-provided
+    project: './tsconfig.eslint.json',
   },
   plugins: ['@typescript-eslint'],
   rules: {},
+  ignorePatterns: ['dist/*'],
 }
