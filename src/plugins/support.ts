8bc6ea27b80e647a67dff552f48cb3761fade1b0
chornos13 <chornos13@hotmail.com>
2022-02-11 00:10:57 +0700

    chore: add eslint

--- src/plugins/support.ts
+++ src/plugins/support.ts
@@ -7,7 +7,7 @@ export interface SupportPluginOptions {
 // The use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
 export default fp<SupportPluginOptions>(async (fastify, opts) => {
-  fastify.decorate('someSupport', function () {
+  fastify.decorate('someSupport', () => {
     return 'hugs'
   })
 })
